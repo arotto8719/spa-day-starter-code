9c848e3e9426450fe86d25ea1816a5f54b015436
arotto8719 <arotto87@gmail.com>
2019-12-12 11:27:08 -0600

    finished studio

--- src/main/java/org/launchcode/spaday/SpaDayApplication.java
+++ src/main/java/org/launchcode/spaday/SpaDayApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpaDayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpaDayApplication.class, args);
 	}
